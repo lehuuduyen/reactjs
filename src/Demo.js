7f2fc28eb810e8fa6ac584b05e54fdbd5365f902
Your Name <admin@Duyenle.local>
2019-08-04 22:08:46 +0700

    update

--- src/Demo.js
+++ src/Demo.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import ColorPicker from './component/settingColor/ColorPicker'
+import SizeSetting from './component/settingColor/SizeSetting'
 
 function Demo() {
   return (
       
-      <div></div>
+    <div className='container mt-50'>
+        <div className='row'>
+          {/* color picker */}
+          <ColorPicker/>
+
+          <SizeSetting/>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <p>Color : red - Fontsize : 15px</p>
+            <div id="content" >
+              Nội dung setting
+            </div>
+          </div>
+        </div>
+      </div>
 
 
   );
---
+++ src/component/settingColor/Reset.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SettingReset(){
+    return (
+             <button type="button" className="btn btn-primary" >reset</button>
+
+
+    );
+
+
+
+}
+export default SettingReset;
