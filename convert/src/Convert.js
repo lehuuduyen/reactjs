e5331df025a7462e70054037d4c39b82571c61ae
ICDVN-DUYENLH\User <lehuuduyen>
2023-09-28 17:01:07 +0700

    up

--- convert/src/Convert.js
+++ convert/src/Convert.js
@@ -15,8 +15,16 @@ function Convert() {
   const props = {
     name: "file",
     multiple: true,
-    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+
+    action: "http://convert.getlinktraffic.space/convert.php",
+    data: {
+      to: "tinyPNG",
+    },
+
     onChange(info) {
+      console.log("====================================");
+      console.log(info);
+      console.log("====================================");
       const { status } = info.file;
       if (status !== "uploading") {
         console.log(info.file, info.fileList);
@@ -74,6 +82,7 @@ function Convert() {
     },
   ]);
   const handleCancel = () => setPreviewOpen(false);
+
   const handlePreview = async (file) => {
     if (!file.url && !file.preview) {
       file.preview = await getBase64(file.originFileObj);
@@ -109,27 +118,63 @@ function Convert() {
         <div class="uk-container uk-text-center">
           <div class="upload-container">
             <div class="upload-container-form">
-              <Dragger {...props}>
-                <p className="ant-upload-drag-icon">
-                  <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">
-                  Click or drag file to this area to upload
-                </p>
-                <p className="ant-upload-hint">
-                  Support for a single or bulk upload. Strictly prohibited from
-                  uploading company data or other banned files.
-                </p>
-              </Dragger>
-              <Upload
-                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+              <Upload.Dragger
+                {...props}
+                itemRender={(originNode, file) => (
+                  <div class="ant-upload-list-item ant-upload-list-item-done">
+                    <a
+                      className="ant-upload-list-item-thumbnail"
+                      href="hhhhhhhhhhhhhhhhhh"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src="hhhhhhhhhhhhh"
+                        alt="cam-trai-du-lich-ho-ba-be-kynghidongduong-000.jpg"
+                        className="ant-upload-list-item-image"
+                      />
+                    </a>
+                    <span
+                      className="ant-upload-list-item-name"
+                      title="cam-trai-du-lich-ho-ba-be-kynghidongduong-000.jpg"
+                    >
+                      cam-trai-du-lich-ho-ba-be-kynghidongduong-000.jpg
+                    </span>
+                    <span className="ant-upload-list-item-actions picture">
+                      <button
+                        title="Remove file"
+                        type="button"
+                        className="ant-btn css-dev-only-do-not-override-1ck3jst ant-btn-text ant-btn-sm ant-btn-icon-only ant-upload-list-item-action"
+                      >
+                        <span className="ant-btn-icon">
+                          <span
+                            role="img"
+                            aria-label="delete"
+                            tabindex="-1"
+                            className="anticon anticon-delete"
+                          >
+                            <svg
+                              viewBox="64 64 896 896"
+                              focusable="false"
+                              data-icon="delete"
+                              width="1em"
+                              height="1em"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"></path>
+                            </svg>
+                          </span>
+                        </span>
+                      </button>
+                    </span>
+                  </div>
+                )}
                 listType="picture"
-                fileList={fileList}
                 onPreview={handlePreview}
-                onChange={handleChange}
               >
-                {fileList.length >= 8 ? null : uploadButton}
-              </Upload>
+                <p>Drag here</p>
+              </Upload.Dragger>
               <Modal
                 open={previewOpen}
                 title={previewTitle}
